16a516da59a26720d498f09f7aa944ef167c5c3d
Etxabe <150168621+Etxabe@users.noreply.github.com>
2025-01-30 18:17:27 +0100

    Modelo funcional de el recibo/envio de json-s

--- twitchAnalytics.php
+++ twitchAnalytics.php
@@ -17,127 +17,214 @@ $opcion_menu = trim(fgets(STDIN));
 
 switch($opcion_menu){
     case "1":
+        //Coger el id del streamer que se desea buscar
         echo "Introduzca el ID del Streamer:";
         $id_usuario = trim(fgets(STDIN));
 
+        //Configurar llamada a la API
         $url = "https://api.twitch.tv/helix/users?id=" . $id_usuario;
-        // Configurar los encabezados
         $headers = [
             "Authorization: Bearer 09pmsrc1ov1mkg0ajinfnd5ty585j0",
             "Client-Id: 3kvc11lm0hiyfqxs32i127986wbep6"
         ];
 
-        // Inicializar cURL
+        //Configurar opciones de cURL
         $ch = curl_init();
-
-        // Configurar opciones de cURL
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
-        // Ejecutar la solicitud
+        //Ejecutar cURL
         $response = curl_exec($ch);
 
-        // Obtener el código de estado HTTP
+        //Obtener el código de estado
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
         // Verificar el código de estado
         if ($httpCode == 200) {
-            echo "✅ Respuesta exitosa (200 OK)\n";
-            //Procesamos el JSON
-            /*$data = json_decode($response, true);
-            $id = $data["id"];
-            $login = $data["user_login"];
-            $username = $data["user_name"];
-            $type = $data["type"];
-            //$broadcaster_type = $data[""];
-            $description = $data["title"];
-            $profile_image_url = $data[""];
-            $offline_image_url = $data[""];
-            $view_count = $data["viewer_count"];
-            $created_at = $data["started_at"];
-*/
-        } else {
-            echo "⚠️ Código de error: $httpCode\n";
-        }
-
-        echo "Codigo de peticion: " . $httpCode;
-        break;
-    case "2":
-        $url = "https://api.twitch.tv/helix/streams";
-        // Configurar los encabezados
-        $headers = [
-            "Authorization: Bearer 09pmsrc1ov1mkg0ajinfnd5ty585j0",
-            "Client-Id: 3kvc11lm0hiyfqxs32i127986wbep6"
-        ];
-
-        // Inicializar cURL
-        $ch = curl_init();
-
-        // Configurar opciones de cURL
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-        // Ejecutar la solicitud
-        $response = curl_exec($ch);
-
-        // Obtener el código de estado HTTP
-        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-        // Verificar el código de estado
-        if ($httpCode == 200) {
-            echo "✅ Respuesta exitosa (200 OK)\n";
             //Procesamos el JSON
             $data = json_decode($response, true);
-            foreach ($data[""] as $stream) {            //Falta encontrar ahi que elemento es en el $data["XXX"]
-                $username = $stream["user_name"];
-                $title = $stream["title"];
+            foreach ($data["data"] as $streamer) {
+                $infoStreamer = [
+                    "id" => $streamer["id"],
+                    "login" => $streamer["login"],
+                    "display_name" => $streamer["display_name"],
+                    "type" => $streamer["type"],
+                    "broadcaster_type" => $streamer["broadcaster_type"],
+                    "description" => $streamer["description"],
+                    "profile_image_url" => $streamer["profile_image_url"],
+                    "offline_image_url" => $streamer["offline_image_url"],
+                    "view_count" => $streamer["view_count"],
+                    "created_at" => $streamer["created_at"]
+                ];
             }
+            //Generamos JSON de envio
+            $jsonFinal = json_encode($infoStreamer, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+
+            echo $jsonFinal;
+        } else {
+            echo "⚠️ Código de error: $httpCode\n";
+        }
+        break;
+
+    case "2":
+        //Configurar llamada a la API
+        $url = "https://api.twitch.tv/helix/streams";
+        $headers = [
+            "Authorization: Bearer 09pmsrc1ov1mkg0ajinfnd5ty585j0",
+            "Client-Id: 3kvc11lm0hiyfqxs32i127986wbep6"
+        ];
+
+        //Configurar opciones de cURL
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        //Ejecutar cURL
+        $response = curl_exec($ch);
+
+        //Obtener el código de estado
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        //Preparar array para crear el json
+        $infoStreams = [];
+
+        //Verificar el código de estado
+        if ($httpCode == 200) {
+            //Procesamos el JSON
+            $data = json_decode($response, true);
+
+            //Recorrer el array para guardar lo que nos interesa
+            foreach ($data["data"] as $stream) {
+                $nuevoStream = [
+                    "title" => $stream["title"],
+                    "user_name" => $stream["user_name"]
+                ];
+
+                $infoStreams[] = $nuevoStream;
+            }
+
+            //Generamos JSON de envio
+            $jsonFinal = json_encode($infoStreams, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+
+            echo $jsonFinal;
 
         } else {
             echo "⚠️ Código de error: $httpCode\n";
         }
 
         break;
+
     case "3":
-        echo "Introduzca el limite de streams:";
-        $limite_streams = trim(fgets(STDIN));
-        //Obtener la informacion de los streams (viene ordenada por num de espectadores)
-        $url = "https://api.twitch.tv/helix/streams";
-        // Configurar los encabezados
-        $headers = [
+        //Coger el limite de streams
+        echo "Introduzca el limite de streams: ";
+        $limit = trim(fgets(STDIN));
+
+        //Paso 1: coger los streams
+        //Configurar llamada a la API
+        $urlStreams = "https://api.twitch.tv/helix/streams";
+        $headersStreams = [
             "Authorization: Bearer 09pmsrc1ov1mkg0ajinfnd5ty585j0",
             "Client-Id: 3kvc11lm0hiyfqxs32i127986wbep6"
         ];
 
-        // Inicializar cURL
-        $ch = curl_init();
+        //Configurar opciones de cURL
+        $chStreams = curl_init();
+        curl_setopt($chStreams, CURLOPT_URL, $urlStreams);
+        curl_setopt($chStreams, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($chStreams, CURLOPT_HTTPHEADER, $headersStreams);
 
-        // Configurar opciones de cURL
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        //Ejecutar cURL
+        $responseStreams = curl_exec($chStreams);
 
-        // Ejecutar la solicitud
-        $response = curl_exec($ch);
+        //Obtener el código de estado
+        $httpCodeStreams = curl_getinfo($chStreams, CURLINFO_HTTP_CODE);
 
-        // Obtener el código de estado HTTP
-        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        //Preparar array para crear el json
+        $infoStreams = [];
 
-        // Verificar el código de estado
-        if ($httpCode == 200) {
-            echo "✅ Respuesta exitosa (200 OK)\n";
+        //Verificar el código de estado
+        if ($httpCodeStreams == 200) {
+            //Procesamos el JSON
+            $dataStreams = json_decode($responseStreams, true);
+
+            //Recorrer el array para guardar lo que nos interesa
+            foreach ($dataStreams["data"] as $stream) {
+                //Limitamos el numero de streams
+                if($limit > 0){
+                    //Paso 2: coger info de streamer para cada stream
+                    $idStreamer = $stream["user_id"];
+
+                    //Configurar llamada a la API
+                    $urlUser = "https://api.twitch.tv/helix/users?id=" . $idStreamer;
+                    $headersUser = [
+                        "Authorization: Bearer 09pmsrc1ov1mkg0ajinfnd5ty585j0",
+                        "Client-Id: 3kvc11lm0hiyfqxs32i127986wbep6"
+                    ];
+
+                    //Configurar opciones de cURL
+                    $chUser = curl_init();
+                    curl_setopt($chUser, CURLOPT_URL, $urlUser);
+                    curl_setopt($chUser, CURLOPT_RETURNTRANSFER, true);
+                    curl_setopt($chUser, CURLOPT_HTTPHEADER, $headersUser);
+
+                    //Ejecutar cURL
+                    $responseUser = curl_exec($chUser);
+
+                    //Obtener el código de estado
+                    $httpCodeUser = curl_getinfo($chUser, CURLINFO_HTTP_CODE);
+
+                    // Verificar el código de estado
+                    if ($httpCodeUser == 200) {
+                        //Procesamos el JSON
+                        $data = json_decode($responseUser, true);
+                        foreach ($data["data"] as $streamer) {
+                            $user_name = $streamer["login"];
+                            $display_name = $streamer["display_name"];
+                            $profile_image_url = $streamer["profile_image_url"];
+                        }
+
+                    } else {
+                        echo "⚠️ Código de error: $httpCode\n";
+                    }
+
+                    $nuevoStreamEnriquecido = [
+                        "stream_id" => $stream["id"],
+                        "user_id" => $idStreamer,
+                        "user_name" => $user_name,
+                        "viewer_count" => $stream["viewer_count"],
+                        "title" => $stream["title"],
+                        "user_display_name" => $display_name,
+                        "profile_image_url" => $profile_image_url
+                    ];
+
+                    // Cerrar la conexión cURL
+                    curl_close($chUser);
+
+                    //Añadir stream
+                    $infoStreamsEnriquecidos[] = $nuevoStreamEnriquecido;
+                    $limit--;
+                }
+            }
+
+            //Generamos JSON de envio
+            $jsonFinal = json_encode($infoStreamsEnriquecidos, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+
+            echo $jsonFinal;
 
         } else {
             echo "⚠️ Código de error: $httpCode\n";
         }
 
-        //Buscar la informacion de los streamers de la lista con su user_id
-        //Mezclar informacion para reenviarla
+        // Cerrar la conexión cURL
+        curl_close($chStreams);
+
+        break;
+
+    default:
+        echo "No es una opcion posible";
         break;
 }
 
-// Cerrar la conexión cURL
-curl_close($ch);
 ?>
