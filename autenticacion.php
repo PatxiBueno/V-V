5f2f9778967c7b43e7680fb556a62bc34b68b219
Etxabe <150168621+Etxabe@users.noreply.github.com>
2025-02-20 12:02:18 +0100

    Funcion de autenticacion

---
+++ autenticacion.php
@@ -0,0 +1,53 @@
+<?php
+
+//Cabecera
+require_once('conexion.php');
+
+function validarToken($headers){
+
+    //Comprobar que la cabecera Authorization existe
+    if (isset($headers['Authorization'])){
+        //Sustrae de la cabecera el "Bearer" y el token
+        list($bearer, $tokenUsuario) = explode(" ", $headers['Authorization'], 2);
+        //Comprueba que la cabecera es correcta y que viene con "bearer" en ella 
+        if (strcasecmp($bearer, "Bearer") == 0) {
+            //Si la cabecera es correcta (Authorization: Bearer ...) comprueba el token
+            /*
+            DESCOMENTAR
+            $con = conexion();
+            $consultaTokens = "SELECT fecha_token FROM token WHERE token LIKE " . $tokenUsuario;
+            $resultado = $con->query($consultaTokens);
+            */
+            $resultado = true;
+            if ($resultado) {
+                //El token es correcto
+                //Comprobar si no ha caducado
+                /*
+                $fila = $resultado->fetch_assoc();
+                $timestampToken = $fila['fecha_token'];
+                //Calculamos que no hayan pasado 3 dias (259200 seundos)
+                $tiempoDelToken = time() - $timestampToken;
+                */
+                $tiempoDelToken = 1;
+                if ($tiempoDelToken <= 259200) {
+                    //El token es valido y no ha expirado
+                    return true;
+                } else {
+                    //El token ha expirado
+                    return false;
+                }
+            } else {
+                //El token es incorrecto
+                return false;
+            }
+        } else {
+            //La cabecera no tiene el formato correcto (falta bearer)
+            return false;
+        }
+    } else {
+        //No existe la cabecera Authorization
+        return false;
+    }
+}
+
+?>
