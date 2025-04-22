2440e5c874221bac5196cd89ce9b282dd4a11bdb
PatxiBueno <bueno.153894@e.unavarra.es>
2025-04-22 18:03:59 +0200

    [#] Cabeceras de debug añadidas

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 	header("Content-type: application/json; charset=utf-8");
-
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
 	require_once('endPoints/get/user.php');
 	require_once('endPoints/get/streams.php');
 	require_once('endPoints/get/topsofthetops.php');
