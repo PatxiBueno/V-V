079eb5b9462e596648f3dbce0fb92c3114e04e9a
joritz <joritzetxenike@gmail.com>
2025-05-18 08:55:31 +0000

    [#36,#42] - refactorizado de register para que funcione con la estructura nueva de archivos

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,26 +1,17 @@
 <?php
+
 require_once __DIR__ . '/../vendor/autoload.php';
-
-(new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
-    dirname(__DIR__)
-))->bootstrap();
-
+(new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(dirname(__DIR__)))->bootstrap();
 date_default_timezone_set(env('APP_TIMEZONE', 'UTC'));
-
-$app = new Laravel\Lumen\Application(
-    dirname(__DIR__)
-);
-
+$app = new Laravel\Lumen\Application(dirname(__DIR__));
 use TwitchAnalytics\Middleware\VerifyToken;
-
 $app->routeMiddleware([
     'auth.token' => VerifyToken::class,
 ]);
 $app->withFacades();
-
 // Registrar rutas
 $app->router->group([], function ($router) {
+
     require __DIR__ . '/../routes/api.php';
 });
-
 return $app;
