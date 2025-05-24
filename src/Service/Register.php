2a2c7791411a06e931edc048dd4b8ac6d06c5be1
Etxabe <150168621+Etxabe@users.noreply.github.com>
2025-05-24 20:17:29 +0200

    [#104] - Creado y comprobado funcionamiento de validador de email (#109)
    * [#104] - Creado y comprobado funcionamiento de validador de email
    
    * [#5104 - Arreglado error phpcs añadiendo linea en blanco al final de EmailValidator.php
    
    * [#104] - Actualizado register(). Ahora la funcion recibe el email saneado

--- src/Service/Register.php
+++ src/Service/Register.php
@@ -8,39 +8,24 @@ use TwitchAnalytics\ResponseTwitchData;
 
 class Register
 {
-    private Request $request;
     private MYSQLDBManager $dbManager;
-    public function __construct($request, $dbManager)
+    public function __construct($dbManager)
     {
-        $this->request = $request;
         $this->dbManager = $dbManager;
     }
-    public function registerUser(): \Illuminate\Http\JsonResponse
+    public function registerUser($data): array
     {
-        $data = $this->request->json()->all();
-        $response = $this->register($data);
-        return response()->json($response['data'], $response['http_code']);
+        return $this->register($data);
     }
-    private function register($data)
+    private function register($email)
     {
-        if (!isset($data["email"])) {
-            return ['data' => ["error" => "The email is mandatory"], 'http_code' => 400];
-        }
-        $email = filter_var($data["email"], FILTER_SANITIZE_EMAIL);
-
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            return ['data' => ["error" => "The email must be a valid email address"], 'http_code' => 400];
-        }
-
         $newApiKey = bin2hex(random_bytes(8));
         $hashedNewApiKey = hash("sha256", $newApiKey);
 
         $user = $this->dbManager->getUserByEmail($email);
 
-        if (!$user) {
-            if (!$this->dbManager->insertUserWithHashedApiKey($email, $hashedNewApiKey)) {
-                return ['data' => ["error" => "Internal server error."], 'http_code' => 500];
-            }
+        if (!$user && !$this->dbManager->insertUserWithHashedApiKey($email, $hashedNewApiKey)) {
+            return ['data' => ["error" => "Internal server error."], 'http_code' => 500];
         }
         if (!$this->dbManager->updateUserHashedKey($hashedNewApiKey, $email)) {
             return ['data' => ["error" => "Internal server error."], 'http_code' => 500];
