7d62f3f3215d76b57317bdf90bcb7565ea059561
Joritz <150168708+Joritzetxenike@users.noreply.github.com>
2025-05-24 22:14:03 +0200

    [#108] - clausulaas de guarda en verifyToken (#114)

--- src/Middleware/VerifyToken.php
+++ src/Middleware/VerifyToken.php
@@ -5,17 +5,51 @@ namespace TwitchAnalytics\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 
-require_once __DIR__ . '/../../autenticacion.php';
-
+require_once __DIR__ . '/../../bbdd/conexion.php';
 class VerifyToken
 {
     public function handle(Request $request, Closure $next)
     {
         $headers = $request->headers->all();
-        if (!validarToken($headers)) {
+        if (!$this->validateToken($headers)) {
             return response()->json(['error' => 'Unauthorized. Token is invalid or expired.'], 401);
         }
 
         return $next($request);
+    }
+
+    public function validateToken($headers)
+    {
+        if (!isset($headers['authorization'])) {
+            return false;
+        }
+
+        list($bearer, $tokenUsuario) = explode(" ", $headers['authorization'][0], 2);
+
+        if (strcasecmp($bearer, "Bearer") != 0) {
+            return false;
+        }
+        $con = conexion();
+        $resultado = $this->getUserTokenFromDataBase($tokenUsuario, $con);
+
+        if (!$resultado || $resultado->num_rows === 0) {
+            return false;
+        }
+
+        $fila = $resultado->fetch_assoc();
+        $timestampToken = $fila['fecha_token'];
+        $tiempoDelToken = time() - strtotime($timestampToken);
+
+        if ($tiempoDelToken > 259200) {
+            return false;
+        }
+        return true;
+    }
+
+    public function getUserTokenFromDataBase(string $userToken, ?\mysqli $con): bool|\mysqli_result
+    {
+        $queryToken = "SELECT fecha_token FROM token WHERE token LIKE  '$userToken'";
+        $resultado = $con->query($queryToken);
+        return $resultado;
     }
 }
