f387e6b10dfb666934e94536e1f437d9c5c5eb53
joritz <joritzetxenike@gmail.com>
2025-05-24 14:07:46 +0000

    [#97] - creado validador topsOfTheTops

--- src/Controllers/TopsOfTheTopsController.php
+++ src/Controllers/TopsOfTheTopsController.php
@@ -5,12 +5,21 @@ namespace TwitchAnalytics\Controllers;
 use TwitchAnalytics\Service\TopsOfTheTops;
 use Illuminate\Http\Request;
 use TwitchAnalytics\Managers\TwitchAPIManager;
+use TwitchAnalytics\Validators\TopsOfTheTopsValidator;
 
 class TopsOfTheTopsController
 {
     public function getTopsOfTheTops(Request $request)
     {
+        $topsValidator = new TopsOfTheTopsValidator();
+        $since = $request->get("since", 600);
+
+        if(!$topsValidator->validateSince($since)) {
+            return response()->json(["error" => "Bad request. Invalid or missing parameters."], 400);
+        }
+
         $topsOfTheTops = new TopsOfTheTops($request, new TwitchAPIManager());
-        return $topsOfTheTops->getTops();
+        $response = $topsOfTheTops->getTops($since);
+        return response()->json($response['data'], $response['http_code']);
     }
 }
