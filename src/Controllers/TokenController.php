686075e1dd01f48236e45b4dc39902aee5ee35d3
Joritz <150168708+Joritzetxenike@users.noreply.github.com>
2025-05-25 09:05:41 +0200

    105 crear validador api key (#111)
    * [#105] - creado validador para api key
    
    * Merge de main a validador api key para tener los cambios para validar email (#110)
    
    * [#105] - creado validador para api key (#106)
    
    Co-authored-by: Patxi <119303981+PatxiBueno@users.noreply.github.com>
    
    * Revert "[#105] - creado validador para api key (#106)"
    
    This reverts commit 08db2d6407ecfbb4f6ea84db6e9f100247c673de.
    
    * [#104] - Creado y comprobado funcionamiento de validador de email (#109)
    
    * [#104] - Creado y comprobado funcionamiento de validador de email
    
    * [#5104 - Arreglado error phpcs añadiendo linea en blanco al final de EmailValidator.php
    
    * [#104] - Actualizado register(). Ahora la funcion recibe el email saneado
    
    ---------
    
    Co-authored-by: Patxi <119303981+PatxiBueno@users.noreply.github.com>
    Co-authored-by: Patxi Bueno <bueno.153894@e.unavarra.es>
    Co-authored-by: Etxabe <150168621+Etxabe@users.noreply.github.com>
    
    * [#105] - arreglo token para validar email
    
    ---------
    
    Co-authored-by: Patxi <119303981+PatxiBueno@users.noreply.github.com>
    Co-authored-by: Patxi Bueno <bueno.153894@e.unavarra.es>
    Co-authored-by: Etxabe <150168621+Etxabe@users.noreply.github.com>

--- src/Controllers/TokenController.php
+++ src/Controllers/TokenController.php
@@ -5,12 +5,32 @@ namespace TwitchAnalytics\Controllers;
 use TwitchAnalytics\Managers\MYSQLDBManager;
 use TwitchAnalytics\Service\Token;
 use Illuminate\Http\Request;
+use TwitchAnalytics\Validators\apiKeyValidator;
+use TwitchAnalytics\Validators\EmailValidator;
 
 class TokenController
 {
     public function getToken(Request $request)
     {
+        $data = $request->json()->all();
+
+        $apikeyValidator = new ApiKeyValidator();
+        $emailValidator = new EmailValidator();
+
+        if (!$apikeyValidator->existsApiKey($data)) {
+            return response()->json(["error" => "The api_key is mandatory"], 400);
+        }
+        if (!$emailValidator->existsEmail($data)) {
+            return response()->json(["error" => "The email is mandatory"], 400);
+        }
+        if (!$emailValidator->emailIsValid($data["email"])) {
+            return response()->json(["error" => "The email must be a valid email address"], 400);
+        }
         $token = new Token($request, new MYSQLDBManager());
-        return $token->genToken();
+        $sanitizedEmail = filter_var($data["email"], FILTER_SANITIZE_EMAIL);
+        $apiKey = $request->get("api_key");
+
+        $response = $token->genToken($sanitizedEmail, $apiKey);
+        return response()->json($response['data'], $response['http_code']);
     }
 }
---
+++ src/Validators/ApiKeyValidator.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace TwitchAnalytics\Validators;
+
+class ApiKeyValidator
+{
+    public function __construct()
+    {
+    }
+
+    public function existsApiKey($data):bool
+    {
+        return isset($data['api_key']);
+    }
+}
