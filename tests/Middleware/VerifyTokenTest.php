222ba4468b27e5d7a3f64cbbf37bc13e4a4ea5b9
Patxi Bueno <bueno.153894@e.unavarra.es>
2025-06-02 16:25:39 +0000

    [#131] - Test en verde para token que no se encuentra en la base de datos

--- tests/Middleware/VerifyTokenTest.php
+++ tests/Middleware/VerifyTokenTest.php
@@ -4,7 +4,6 @@ namespace TwitchAnalytics\Tests\Middleware;
 
 use \Illuminate\Http\Request;
 use Mockery;
-use Mockery\Mock;
 use TwitchAnalytics\Managers\MYSQLDBManager;
 use TwitchAnalytics\Middleware\VerifyToken;
 use PHPUnit\Framework\TestCase;
@@ -36,6 +35,27 @@ class VerifyTokenTest extends TestCase
         $request = new Request();
         $request->headers->set('Authorization', 'InvalidTokenFormat token');
         $dbManager = Mockery::mock(MYSQLDBManager::class);
+        $tokenVerifier = new VerifyToken($dbManager);
+        $response = $tokenVerifier->handle($request, fn () => null);
+
+        $this->assertEquals(401, $response->getStatusCode());
+        $this->assertEquals(
+            json_encode(['error' => 'Unauthorized. Token is invalid or expired.']),
+            $response->getContent()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function requestWithNonExistentTokenIsRejected()
+    {
+        $request = new Request();
+        $request->headers->set('Authorization', 'Bearer invalid_token');
+        $dbManager = Mockery::mock(MYSQLDBManager::class);
+        $dbManager->shouldReceive('getExpirationDayOfToken')
+            ->with('invalid_token')
+            ->andReturn(null);
         $tokenVerifier = new VerifyToken($dbManager);
         $response = $tokenVerifier->handle($request, fn () => null);
 
