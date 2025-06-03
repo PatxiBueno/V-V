901a1967d962b92cebdeee79b83624cc65649e91
Aritz <aritzhu95@gmail.com>
2025-06-03 19:25:31 +0200

    [#140] - Creacion de tests de integración para endpoint tops of the tops (#148)
    * [#135] - Test de integracion 400 para Token, missing ApiKey
    
    * [#135] - Test de integracion 400 para Token, missing mail given ApiKey
    
    * [#135] - Test de integracion 400 para Token, wrong mail, given mail and apikey
    
    * [#135] - Test de integracion 401 para Token, not authorized ApiKey
    
    * [#135] - Test de integracion happy path 200 para Token
    
    * [#140] - Test para Tops of the tops, caso de invalid since
    
    * [#140] - Test para Tops of the tops, happy path recogida de datos desde cache
    
    * [#140] - Test para Tops of the tops, happy path recibiendo los datos de la api

---
+++ tests/Integration/Controllers/TopsOfTheTopsEndPointTest.php
@@ -0,0 +1,161 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TwitchAnalytics\Tests\Integration\Controllers;
+
+use TwitchAnalytics\Managers\MYSQLDBManager;
+use TwitchAnalytics\Managers\TwitchAPIManager;
+use TwitchAnalytics\ResponseTwitchData;
+use TwitchAnalytics\Service\TopsOfTheTops;
+use DateTime;
+use Mockery;
+use TwitchAnalytics\Controllers\TopsOfTheTopsController;
+use TwitchAnalytics\Validators\TopsOfTheTopsValidator;
+use PHPUnit\Framework\TestCase;
+use Illuminate\Http\Request;
+
+class TopsOfTheTopsEndPointTest extends TestCase
+{
+    private TopsOfTheTopsController $topsController;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    /**
+     * @test
+     *
+     */
+    public function invalidParameterSinceErrorCode400(): void
+    {
+        $postData = ['since' => '-1'];
+        $json = json_encode($postData);
+
+        $server = [
+            'REQUEST_METHOD' => 'POST',
+            'REQUEST_URI'    => '/token',
+            'CONTENT_TYPE'   => 'application/json',
+        ];
+
+        $request = new Request($postData, [], [], [], [], $server, $json);
+        $mysqlManager = mock(MYSQLDBManager::class);
+        $apiManager = mock(TwitchAPIManager::class);
+        $topsOfTheTopsService = new TopsOfTheTops($apiManager, $mysqlManager);
+        $this->topsController = new TopsOfTheTopsController(new TopsOfTheTopsValidator(), $topsOfTheTopsService);
+
+        $response = $this->topsController->getTopsOfTheTops($request);
+        $responseData = json_decode($response->getContent(), true);
+
+        $this->assertEquals(400, $response->getStatusCode());
+        $this->assertEquals([
+            'error' => "Bad request. Invalid or missing parameters.",
+            ], $responseData);
+    }
+
+    /**
+     * @test
+     *
+     */
+    public function happyPahtForCache204(): void
+    {
+        $postData = ['since' => '600'];
+        $json = json_encode($postData);
+
+        $server = [
+            'REQUEST_METHOD' => 'POST',
+            'REQUEST_URI'    => '/token',
+            'CONTENT_TYPE'   => 'application/json',
+        ];
+
+        $request = new Request($postData, [], [], [], [], $server, $json);
+
+        $mysqlManager = mock(MYSQLDBManager::class);
+        $mysqlManager
+        ->shouldReceive('getCacheInsertTime')
+        ->andReturn([
+            'fecha_insercion' => '2055-06-03 14:23:45',
+            ]);
+
+        $mysqlManager
+        ->shouldReceive('getTopsCacheData')
+        ->andReturn([]);
+        $apiManager = mock(TwitchAPIManager::class);
+        $topsOfTheTopsService = new TopsOfTheTops($apiManager, $mysqlManager);
+        $this->topsController = new TopsOfTheTopsController(new TopsOfTheTopsValidator(), $topsOfTheTopsService);
+
+        $response = $this->topsController->getTopsOfTheTops($request);
+
+        $this->assertEquals(204, $response->getStatusCode());
+    }
+
+    /**
+     * @test
+     *
+     */
+    public function happyPahtForApi200(): void
+    {
+        $postData = ['since' => '600'];
+        $json = json_encode($postData);
+
+        $server = [
+            'REQUEST_METHOD' => 'POST',
+            'REQUEST_URI'    => '/token',
+            'CONTENT_TYPE'   => 'application/json',
+        ];
+
+        $request = new Request($postData, [], [], [], [], $server, $json);
+
+        $mysqlManager = mock(MYSQLDBManager::class);
+        $mysqlManager
+        ->shouldReceive('getCacheInsertTime')
+        ->andReturn([
+            'fecha_insercion' => '2005-06-03 14:23:45',
+        ]);
+
+        $mysqlManager
+        ->shouldReceive('cleanTopOfTheTopsCache()')
+        ->andReturn(true);
+
+        $mysqlManager
+        ->shouldReceive('curlToTwitchApiForTopThreeGames')
+        ->andReturn(new ResponseTwitchData(200, json_encode([[
+            "game_id" => "509658",
+            "game_name" => "Just Chatting",
+            "userName" => "KaiCenat",
+            "totalVideos" => 36,
+            "totalViews" => 100000,
+            "mostTitle" => "Funny Moments",
+            "mostViews" => 45000,
+            "mostDuration" => "1h 10m",
+            "mostDate" => "2025-06-03 14:23:45"
+        ]])));
+        $mysqlManager
+        ->shouldReceive('cleanTopOfTheTopsCache')
+        ->andReturn(true);
+
+        $mysqlManager
+        ->shouldReceive('insertTopsCache')
+        ->andReturn(true);
+
+        $apiManager = mock(TwitchAPIManager::class);
+        $apiManager
+        ->shouldReceive('curlToTwitchApiForTopThreeGames')
+        ->andReturn(new ResponseTwitchData(200, json_encode([[
+            "game_id" => "509658",
+            "game_name" => "Just Chatting"
+        ]])));
+        $topsOfTheTopsService = new TopsOfTheTops($apiManager, $mysqlManager);
+        $this->topsController = new TopsOfTheTopsController(new TopsOfTheTopsValidator(), $topsOfTheTopsService);
+
+        $response = $this->topsController->getTopsOfTheTops($request);
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+}
