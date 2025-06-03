b335a5219f91a392400d6e5ad97457a18e135318
Patxi <119303981+PatxiBueno@users.noreply.github.com>
2025-06-03 16:53:12 +0200

    [#136] - Cobertura basica para emails validos e invalidos (#138)

---
+++ tests/Validators/EmailValidatorTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace TwitchAnalytics\Tests\Validators;
+
+use TwitchAnalytics\Validators\EmailValidator;
+use PHPUnit\Framework\TestCase;
+
+class EmailValidatorTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function invalidEmailIsNotValidated(): void
+    {
+        $emailValidator = new EmailValidator();
+        $data = ["email" => "invalid-email"];
+        $this->assertFalse($emailValidator->emailIsValid($data["email"]));
+    }
+
+    /**
+     * @test
+     */
+    public function validEmailIsValidated(): void
+    {
+        $emailValidator = new EmailValidator();
+        $data = ["email" => "valid@gmail.com"];
+        $this->assertTrue($emailValidator->emailIsValid($data["email"]));
+    }
+}
