739f830fbf61837741f106a2e514f482673bfb91
Patxi <119303981+PatxiBueno@users.noreply.github.com>
2025-06-03 09:04:41 +0200

    [#131] - Crear tests unitarios middleware (#134)
    * [#131] - Creados test para tokens nulos y formatos invalidos en las cabeceras
    
    * [#131] - Test en verde para token que no se encuentra en la base de datos
    
    * [#131] - Extraido metodo setUp para inicializar verifyer y mock del dbManager
    
    * [#131] - Happy path testeado pasa en verde
    
    * [#131] - Corregido valid_token en la cabecera del test happy path

---
+++ tests/Middleware/VerifyTokenTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace TwitchAnalytics\Tests\Middleware;
+
+use \Illuminate\Http\Request;
+use Mockery;
+use TwitchAnalytics\Managers\MYSQLDBManager;
+use TwitchAnalytics\Middleware\VerifyToken;
+use PHPUnit\Framework\TestCase;
+
+class VerifyTokenTest extends TestCase
+{
+    private $dbManager;
+    private $tokenVerifyer;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->dbManager = Mockery::mock(MYSQLDBManager::class);
+        $this->tokenVerifyer = new VerifyToken($this->dbManager);
+    }
+    /**
+     * @test
+     */
+    public function requestWithoutTokenIsRejected()
+    {
+        $request = new Request();
+        $response = $this->tokenVerifyer->handle($request, fn () => null);
+
+        $this->assertEquals(401, $response->getStatusCode());
+        $this->assertEquals(
+            json_encode(['error' => 'Unauthorized. Token is invalid or expired.']),
+            $response->getContent()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function requestWithInvalidTokenFormatIsRejected()
+    {
+        $request = new Request();
+        $request->headers->set('Authorization', 'InvalidTokenFormat token');
+
+        $response = $this->tokenVerifyer->handle($request, fn () => null);
+
+        $this->assertEquals(401, $response->getStatusCode());
+        $this->assertEquals(
+            json_encode(['error' => 'Unauthorized. Token is invalid or expired.']),
+            $response->getContent()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function requestWithNonExistentTokenIsRejected()
+    {
+        $request = new Request();
+        $request->headers->set('Authorization', 'Bearer invalid_token');
+        $this->dbManager->shouldReceive('getExpirationDayOfToken')
+            ->with('invalid_token')
+            ->andReturn(null);
+
+        $response = $this->tokenVerifyer->handle($request, fn () => null);
+
+        $this->assertEquals(401, $response->getStatusCode());
+        $this->assertEquals(
+            json_encode(['error' => 'Unauthorized. Token is invalid or expired.']),
+            $response->getContent()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function requestWithExpiredTokenIsRejected()
+    {
+        $request = new Request();
+        $request->headers->set('Authorization', 'Bearer expired_token');
+        $invalidDate = date('Y-m-d H:i:s', time() - (4 * 24 * 3600));
+        $this->dbManager->shouldReceive('getExpirationDayOfToken')
+            ->with('expired_token')
+            ->andReturn(["fecha_token" => $invalidDate]);
+
+        $response = $this->tokenVerifyer->handle($request, fn () => null);
+
+        $this->assertEquals(401, $response->getStatusCode());
+        $this->assertEquals(
+            json_encode(['error' => 'Unauthorized. Token is invalid or expired.']),
+            $response->getContent()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function requestWithValidTokenIsAccepted()
+    {
+        $request = new Request();
+        $request->headers->set('Authorization', 'Bearer valid_token');
+        $validDate = date('Y-m-d H:i:s', time() - (2 * 24 * 3600));
+        $this->dbManager->shouldReceive('getExpirationDayOfToken')
+            ->with('valid_token')
+            ->andReturn(["fecha_token" => $validDate]);
+
+        $called = false;
+        $this->tokenVerifyer->handle($request, function () use (&$called) {
+            $called = true;
+        });
+
+        $this->assertTrue($called);
+    }
+
+    protected function tearDown(): void
+    {
+        Mockery::close();
+        parent::tearDown();
+    }
+}
