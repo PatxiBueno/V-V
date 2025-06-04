5756ed4eb36495677cc5b69d25e0d2f7be8a384c
aritzhu <aritzhu95@gmail.com>
2025-06-04 13:04:12 +0000

    [#163] - Tests de integracion para tops of the tops happy path API

--- tests/Feature/TopsOfTheTopsEndPointIntegration.php
+++ tests/Feature/TopsOfTheTopsEndPointIntegration.php
@@ -49,7 +49,7 @@ class TopsOfTheTopsEndPointIntegration extends TestCase
      * @test
      *
      */
-    public function happyPahtForCache204(): void
+    public function happyPahtForCache200(): void
     {
         $mockDbManager = \Mockery::mock(MYSQLDBManager::class);
         $mockDbManager->shouldReceive('getExpirationDayOfToken')
@@ -73,7 +73,67 @@ class TopsOfTheTopsEndPointIntegration extends TestCase
         $response = $this->call('GET', '/analytics/topsofthetops?since=1', [], [], [], [
             'HTTP_Authorization' => 'Bearer fakeInvalidToken',
         ]);
-        $responseData = json_decode($response->getContent(), true);
+
+        $this->assertEquals(200, $response->status());
+    }
+    /**
+     * @test
+     *
+     */
+    public function happyPahtForApi200(): void
+    {
+        $mockDbManager = \Mockery::mock(MYSQLDBManager::class);
+        $mockDbManager->shouldReceive('getExpirationDayOfToken')
+            ->with('fakeInvalidToken')
+            ->andReturn([
+                'fecha_token' => date('Y-m-d H:i:s', time() - 3600),
+            ]);
+        
+        $mockDbManager
+        ->shouldReceive('getCacheInsertTime')
+        ->andReturn([
+            'fecha_insercion' => '2005-06-03 14:23:45',
+        ]);
+
+        $mockDbManager
+        ->shouldReceive('cleanTopOfTheTopsCache')
+        ->andReturn(true);
+
+        $mockDbManager
+        ->shouldReceive('curlToTwitchApiForTopThreeGames')
+        ->andReturn(new ResponseTwitchData(200, json_encode([[
+            "game_id" => "509658",
+            "game_name" => "Just Chatting",
+            "userName" => "KaiCenat",
+            "totalVideos" => 36,
+            "totalViews" => 100000,
+            "mostTitle" => "Funny Moments",
+            "mostViews" => 45000,
+            "mostDuration" => "1h 10m",
+            "mostDate" => "2025-06-03 14:23:45"
+        ]])));
+        $mockDbManager
+        ->shouldReceive('cleanTopOfTheTopsCache')
+        ->andReturn(true);
+
+        $mockDbManager
+        ->shouldReceive('insertTopsCache')
+        ->andReturn(true);
+
+        $twitchAPIManagerMock = mock(TwitchAPIManager::class);
+        $twitchAPIManagerMock
+        ->shouldReceive('curlToTwitchApiForTopThreeGames')
+        ->andReturn(new ResponseTwitchData(200, json_encode([[
+            "game_id" => "509658",
+            "game_name" => "Just Chatting"
+        ]])));
+
+        $this->app->instance(TokenVerifyer::class, new TokenVerifyer($mockDbManager));
+        $this->app->instance(TwitchAPIManager::class, $twitchAPIManagerMock);
+
+        $response = $this->call('GET', '/analytics/topsofthetops?since=600', [], [], [], [
+            'HTTP_Authorization' => 'Bearer fakeInvalidToken',
+        ]);
 
         $this->assertEquals(200, $response->status());
     }
