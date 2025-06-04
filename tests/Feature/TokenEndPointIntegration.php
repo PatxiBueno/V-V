2e35e8553d34cea2a1313d810deeec2fe9ce81e7
Ander Etxabe <etxabin@gmail.com>
2025-06-04 14:06:24 +0000

    [#168] - Crear tests de integracion token, no api key

---
+++ tests/Feature/TokenEndPointIntegration.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace TwitchAnalytics\Tests\Feature;
+
+use Laravel\Lumen\Testing\TestCase;
+use TwitchAnalytics\Controllers\TokenController;
+use TwitchAnalytics\Managers\MYSQLDBManager;
+use TwitchAnalytics\Managers\TwitchAPIManager;
+use TwitchAnalytics\Middleware\TokenVerifyer;
+use TwitchAnalytics\ResponseTwitchData;
+
+class TokenEndPointIntegration extends TestCase
+{
+    /**
+     * @return \Laravel\Lumen\Application
+     */
+    public function createApplication()
+    {
+        return require __DIR__ . '/../../bootstrap/app.php';
+    }
+
+    /**
+     * @test
+     **/
+    public function noApiKeyProvidedErrorCode400()
+    {
+        $json = json_encode(['email' => 'motto@gmial.com']);
+        $response = $this->call('POST', 'token', [], [], [], [], $json);
+        $responseData = json_decode($response->getContent(), true);
+
+        $this->assertEquals(400, $response->status());
+        $this->assertEquals([
+            "error" => "The api_key is mandatory",
+        ], $responseData);
+    }
+
+    /**
+     * @test
+     **/
+    public function no()
+    {
+
+    }
+
+}
