a9a34791e170e79fd83a48492e3d98671ff62853
Ander Etxabe <etxabin@gmail.com>
2025-06-05 09:20:36 +0000

    [#174] - Crear Tests Unitarios de validators, enriched validator

--- tests/Validators/EnrichedValidatorTest.php
+++ tests/Validators/EnrichedValidatorTest.php
@@ -3,8 +3,31 @@
 namespace TwitchAnalytics\Tests\Validators;
 
 use PHPUnit\Framework\TestCase;
+use TwitchAnalytics\Validators\EnrichedValidator;
 
 class EnrichedValidatorTest extends TestCase
 {
+    private EnrichedValidator $validator;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->validator = new EnrichedValidator();
+    }
+
+    /**
+     * @test
+     **/
+    public function validLimitProvidedReturnsTrue()
+    {
+        $this->assertFalse($this->validator->isValidLimit(-4));
+    }
+
+    /**
+     * @test
+     **/
+    public function invalidLimitProvidedReturnsFalse()
+    {
+        $this->assertTrue($this->validator->isValidLimit(3));
+    }
 
 }
